58288aa087ce4d7407fd95be5765e23cb82200b4
Gustavo Henrique <igustahenrique@gmail.com>
2025-09-04 09:24:59 -0300

    Criação do modal no utils

--- app/utils.php
+++ app/utils.php
@@ -2,6 +2,38 @@
 
 class Util
 {
- public function 
+    public static function modalError($titulo, $conteudo, $id = null)
+    {
+        if (!$id) {
+            $id = 'modal_' . uniqid();
+        }
+
+        $modal = <<<HTML
+        <div class="modal fade" tabindex="-1" id="{$id}" aria-hidden="true" aria-labelledby="{$id}Label">
+            <div class="modal-dialog modal-dialog-centered m-auto d-flex justify-content-center">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="{$id}Label">{$titulo}</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
+                    </div>
+                    <div class="modal-body">
+                        {$conteudo}
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            var myModal = new bootstrap.Modal(document.getElementById('{$id}'));
+                myModal.show();
+            });
+        </script>
+        HTML;
+
+        return $modal;
+    }
 }
 
